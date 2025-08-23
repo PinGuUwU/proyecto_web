134ba1e29dfaaf6b5cd630910b7c79ab21947e3a
PC_Principal <prisci_redon29@hotmail.com>
2025-08-22 22:39:08 -0300

    Inicio y Registro
    Ahora se guarda información de forma local para el inicio y el cierre de sesión

--- js/index.js
+++ js/index.js
@@ -1,18 +1,68 @@
 //Ubicacion
 
 const btnRegistro = document.querySelector('#registrar a')
+const btnRegistrarse = document.querySelector('#btn-registro')
 const presentacionRegistro = document.querySelector('#presentacion-registro')
 const formularioRegistro = document.querySelector('#formulario-registro')
 
+
 const btnInicio = document.querySelector('#iniciar-sesion a')
+const btnIniciarSesion = document.querySelector('#btn-login')
 const presentacionInicio = document.querySelector('#presentacion-inicio')
 const formularioInicio = document.querySelector('#formulario-inicio')
 
+
+
 //Declaraciones
+
+let user = {
+        username: 'usuario',
+        email: 'email',
+        password: 'password'
+    }
 
 function irArriba(){
     window.scrollTo(0,0)
 }
+
+function registrarUsuario(usuario, email, password){
+    user = {
+        username: usuario,
+        email: email,
+        password: password
+    }
+    if(buscarUsuario(user.username) != null){
+        alert('El Usuario o email ingresados ya están registrados.')
+        irInicioSesion()
+    } else {
+        localStorage.setItem('user', JSON.stringify(user))
+        console.log('Se guardaron los datos del usuario: ', user)
+    }
+}
+
+function buscarUsuario(usuario){
+    let usuarioEncontrado = JSON.parse(localStorage.getItem("usuario"))
+        if(usuarioEncontrado != null){
+        console.log('buscarUsuario() encontró un usuario')
+        return usuarioEncontrado
+    } else {
+        console.log('No se encontró el usuario', usuarioEncontrado)
+        return null
+    }
+}
+
+function irInicioSesion(){
+    presentacionRegistro.style.display = 'none'
+    formularioRegistro.style.display = 'none'
+    presentacionInicio.style.display = 'block'
+    formularioInicio.style.display = 'block'
+}
+
+localStorage.setItem('user', JSON.stringify(user))
+
+let usuarioEncontrado = JSON.parse(localStorage.getItem("user"))
+
+console.log(usuarioEncontrado.username)
 
 //control
 
@@ -31,13 +81,36 @@ btnRegistro.addEventListener(
     }
 )
 
+
+
 btnInicio.addEventListener(
     'click', () => {
-        presentacionRegistro.style.display = 'none'
-        formularioRegistro.style.display = 'none'
-        presentacionInicio.style.display = 'block'
-        formularioInicio.style.display = 'block'
+        irInicioSesion()
         irArriba()
+    }
+)
+
+btnRegistrarse.addEventListener(
+    'click', () => {
+        registrarUsuario(
+            document.querySelector('#username-registro input').value,
+            document.querySelector('#email-registro input').value,
+            document.querySelector('#password-registro input').value
+        )
+    }
+)
+
+btnIniciarSesion.addEventListener(
+    'click', () => {
+        const password = document.querySelector('#password-inicio input')
+        const name = document.querySelector('#dato-inicio input')
+        user = buscarUsuario(name)
+        if(user.password != password){
+            alert('La contraseña o el usuario son incorrectos.')
+        } else{
+            console.log('se inicio sesion')
+            
+        }
     }
 )
 
